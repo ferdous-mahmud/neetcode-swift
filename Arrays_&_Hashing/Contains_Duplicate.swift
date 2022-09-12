ff400e1b3f60b3563b61efc7d0a848016ba66d84
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-09-12 21:18:15 +0600

    first commit

---
+++ Arrays_&_Hashing/Contains_Duplicate.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var uniqueElements = Set<Int>()
+    
+        for num in nums {
+            if uniqueElements.contains(num){
+                return true
+            }else{
+                uniqueElements.insert(num)
+            }
+        }
+        return false
+    }
+}

fa424d303f5b2ccdd0b0183c805e11c96f010fd9
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-09-12 21:19:48 +0600

    time and space complexity added

--- Arrays_&_Hashing/Contains_Duplicate.swift
+++ Arrays_&_Hashing/Contains_Duplicate.swift
@@ -1,3 +1,13 @@
+/*
+    Given int array, return true if any value appears at least twice
+    Ex. nums = [1,2,3,1] -> true, nums = [1,2,3,4] -> false
+
+    If seen num previously then has dupe, else insert into hash set
+
+    Time: O(n)
+    Space: O(n)
+*/
+
 class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
         var uniqueElements = Set<Int>()

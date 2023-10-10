24473fb3165e1b86ca74a3d922e30e80b386aa2c
ferdous-mahmud <ferdous.webdev@gmail.com>
2023-10-10 20:15:19 +0600

    Contain duplicate added

---
+++ Arrays_and_hashing/containDuplicate.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        let set = Set(nums)
+        return set.count != nums.count
+    }
+}
+
+let sol = Solution()
+
+if let input = readLine() {
+    if let data = input.data(using: .utf8) {
+        let decoder = JSONDecoder()
+        let numbers = try decoder.decode([Int].self, from: data)
+            
+        let output = sol.containsDuplicate(numbers)
+        print(output)
+    } else {
+        print("Could not convert file contents to data.")
+    }
+}
+
+        
--- Arrays_and_hashing/valid_anagram.swift
+++
@@ -1,8 +0,0 @@
-// Time complexity: O(n log n)
-// Space complexity: O(1)
-
-class Solution {
-    func isAnagram(_ s: String, _ t: String) -> Bool {
-        return t.sorted() == s.sorted()
-    }
-}
